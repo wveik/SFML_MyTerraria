6b414655f7c71c0e56598d6610a494a21dce25eb
wveik <kataev_06@list.ru>
2017-04-28 16:56:49 +0300

    Add project files.

---
+++ MyTerraria/Program.cs
@@ -0,0 +1,31 @@
+﻿using SFML.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyTerraria {
+    class Program {
+        private static RenderWindow win;
+        static void Main(string[] args) {
+            win = new RenderWindow(new SFML.Window.VideoMode(800, 600), "Моя Terraria!");
+            win.SetVerticalSyncEnabled(true);
+
+            win.Closed += Win_Closed;
+
+            while (win.IsOpen) {
+                win.DispatchEvents();
+
+                win.Clear(Color.Black);
+
+                win.Display();
+            }
+        }
+
+        private static void Win_Closed(object sender, EventArgs e) {
+            if (win != null)
+                win.Close();
+        }
+    }
+}
